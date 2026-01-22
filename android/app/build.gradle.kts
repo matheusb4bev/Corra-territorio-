1d8c151c3a98aca4f573aa8235fd9d56540118c6
matheusb4bev <matheusb4bev@gmail.com>
2026-01-22 16:03:31 -0300

    Update build.gradle.kts
    Fix namespace and MainActivity path

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,45 +1,12 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
-
 android {
-    namespace = "com.seujogo.territoryrunner"
+    namespace = "com.corraterritorio.app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
 
     defaultConfig {
         applicationId = "com.corraterritorio.app"
         minSdk = 21
         targetSdk = 34
-
-        // DEFINIDO MANUALMENTE (CORREÇÃO DO ERRO)
         versionCode = 1
         versionName = "1.0.0"
-
-        ndk {
-            abiFilters.add("arm64-v8a")
-        }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        // aviso de deprecated não quebra build
-        jvmTarget = "17"
-    }
-
-    buildTypes {
-        getByName("release") {
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter {
-    source = "../.."
 }
